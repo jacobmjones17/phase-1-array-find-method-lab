e1db811f3bdaae78f8698a45ed4a416a09da9eb1
Jacob <jacobmjones17@gmail.com>
2021-12-06 17:24:00 -0500

    commit tree 4fb323fe4f11c586cd8e760b02fca70dd15beade

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function superbowlWin(record){
+    const result = record.find(r => r.result === "W");
+    if (result) {
+        return result.year 
+    } 
+    else return undefined
+}
 
+// function superbowlWin(record){
+//     for (const r of record) {
+//         if (r.result === "W"){
+//         return r.year
+//     }
+//     else return undefined
+// }
+// }
+    
